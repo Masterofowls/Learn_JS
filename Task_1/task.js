f2ae847ab28d23adbdc0a137a724d19f38254224
Shtarkman Daniel <mrdaniilsh@proton.me>
2026-07-24 23:37:58 +0300

    2

--- Task_1/task.js
+++ Task_1/task.js
@@ -12,3 +12,36 @@ console.log(name, number, isLogic, value, surname)
 console.log(typeof null); // -> object
 console.log(typeof []); // -> object
 console.log(typeof 42 / 0); // -> NaN
+
+/* 3 */
+
+let a = '100';
+let b = 100;
+let c = true;
+
+console.log(typeof a === 'string');  
+
+console.log(typeof b === 'number'); 
+
+console.log(typeof c === 'boolean'); 
+
+/* 4 */
+
+console.log('5' + 3); // -> 53
+console.log('5' - 3); // -> 2
+console.log(true + 1); // -> true
+console.log(false + '1'); // -> false1
+console.log('10' / '2'); // -> 5
+console.log('2' * '3'); // -> 6
+
+/* 5 */
+
+let strNum = '123';
+let boolVal = true;
+let emptyStr = '';
+
+console.log(typeof(Number(strNum)));
+console.log(typeof(Number(boolVal)));
+console.log(typeof(Boolean(emptyStr)));
+console.log(typeof(String(0)));
+console.log(typeof(String(255)));
