f45224bf72adf51a185d96db9f3cd5fec91ea4f6
Shtarkman Daniel <mrdaniilsh@proton.me>
2026-07-24 23:07:08 +0300

    first commit

---
+++ Task_1/task.js
@@ -0,0 +1,14 @@
+/* 1 */
+let name = "";
+let number = 33;
+let isLogic = true;
+let value = null;
+let surname ;
+
+console.log(name, number, isLogic, value, surname)
+
+/* 2 */
+
+console.log(typeof null); // -> object
+console.log(typeof []); // -> object
+console.log(typeof 42 / 0); // -> NaN
