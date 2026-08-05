001ed10dea3e94d9d9e0b86627390c7a2cb14def
Shtarkman Daniel <mrdaniilsh@proton.me>
2026-08-05 07:35:17 +0300

    7

--- Task_2/task.js
+++ Task_2/task.js
@@ -10,6 +10,23 @@ user.age = 26
 delete user.isAdmin
 console.log(user)
 
-//2
+//1.2
 
-function buildObject(key, value)
+function buildObject(key, value) {
+    return { [key]: value };
+}
+buildObject()
+
+// 1.3
+
+const car = { brand: "Toyota", year: 2020, color: "red" }
+function hasProp(obj, prop) {
+    if (prop in obj) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(hasProp(car, 'brand'));  
+console.log(hasProp(car, 'model'));  
+
